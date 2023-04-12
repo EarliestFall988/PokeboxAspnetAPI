5b261c1ba71b1b06d4078711023a4f3f41e0b1bc
Jacob-Armm <jdlegg@ksu.edu>
2023-04-12 17:18:04 -0500

    MOREERER SQL

--- SQL/SelectProcedures.sql
+++ SQL/SelectProcedures.sql
@@ -56,7 +56,7 @@ CREATE OR ALTER PROCEDURE Pokebox.SelectAllPokemonOwnedByUser
     @Username NVARCHAR(32)
 AS
 
-SELECT PO.Name, PO.[Level], PO.Gender, P.PokemonName, POT.PokemonTypeName, PO.DatePutInBox
+SELECT PO.Name, PO.[Level], PO.Gender, P.PokemonName, POT.PokemonTypeName, P.IsLegendary, PO.DatePutInBox
 FROM [User] U
     INNER JOIN PokeOwned PO ON PO.UserID = U.UserID
     INNER JOIN Pokemon P ON P.PokemonID = PO.PokemonID
