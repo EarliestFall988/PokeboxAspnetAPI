3e4c7f5949c4aecdf6a7f7dd48920b1a2072b84b
Jacob-Armm <jdlegg@ksu.edu>
2023-04-12 08:57:22 -0500

    More SQL YIPPEEEEE

--- SQL/Queries.sql
+++ SQL/Queries.sql
@@ -10,13 +10,27 @@ EXEC Pokebox.AddPokeOwned N'a', N'Emma', N'Quirkiest White Bitch', 'N', 69
 
 EXEC Pokebox.AddPokemonType N'Fire'
 
-EXEC Pokebox.AddPokeType N'Fire', N'Taylor'
+EXEC Pokebox.AddPokemonType N'Sassy'
+
+EXEC Pokebox.AddPokeType N'Sassy', N'Taylor'
+
+EXEC Pokebox.AddPokeType N'Fire', N'Emma'
 
 EXEC Pokebox.AddItemType N'Pokeball'
 
+EXEC Pokebox.AddItemType N'Potion'
+
 EXEC Pokebox.AddItem N'Pokeball', N'Safari Ball', N'It catches things'
 
+EXEC Pokebox.AddItem N'Pokeball', N'Penis Ball', N'It catches things'
+
+EXEC Pokebox.AddItem N'Potion', N'Health Potion', N'It heals things'
+
 EXEC Pokebox.AddItemOwned N'a', N'Safari Ball'
+
+EXEC Pokebox.AddItemOwned N'a', N'Penis Ball'
+
+EXEC Pokebox.AddItemOwned N'a', N'Health Potion'
 
 SELECT *
 FROM Pokebox.[User]
@@ -43,5 +57,9 @@ SELECT *
 FROM Pokebox.Item
 
 EXEC Pokebox.SelectAllPokemonOwnedByUser N'a'
+
+EXEC Pokebox.SelectAllItemsOwnedByUser N'a'
+
+EXEC Pokebox.RemoveItemOwned N'a', N'Health Potion'
 
 EXEC Pokebox.SelectAllItemsOwnedByUser N'a'
