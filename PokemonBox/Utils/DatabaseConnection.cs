d50d8b2b4991c3b1362cbabeca0a4b5bb02155cd
Emma Beatty <enbeatty@ksu.edu>
2023-04-19 17:10:19 -0500

    Put all the stuff into DatabaseConnection

--- PokemonBox/Utils/DatabaseConnection.cs
+++ PokemonBox/Utils/DatabaseConnection.cs
@@ -10,14 +10,14 @@ namespace PokemonBox.Utils
 
         const string connectionString = @"Server=(localdb)\MSSQLLocalDb;Database=PokemonBoxDatabase;Integrated Security=SSPI;";
 
-        private static SqlItemTypeRepository ItemTypeRepo;
-        private static TransactionScope transaction;
-
+        public static SqlItemRepository ItemRepo = new SqlItemRepository(connectionString);
+        public static SqlItemsOwnedRepository ItemsOwnedRepo = new SqlItemsOwnedRepository(connectionString);
+        public static SqlItemTypeRepository ItemTypeRepo = new SqlItemTypeRepository(connectionString);
+        public static SqlPokemonRepository PokemonRepo = new SqlPokemonRepository(connectionString);
+        public static SqlPokemonTypeRepository PokemonTypeRepo = new SqlPokemonTypeRepository(connectionString);
+        public static SqlPokeOwnedRepository PokeOwnedRepo = new SqlPokeOwnedRepository(connectionString);
+        public static SqlPokeTypeRepository PokeTypeRepo = new SqlPokeTypeRepository(connectionString);
         public static SqlUserRepository UserRepo = new SqlUserRepository(connectionString);
 
-        public static string GetUser(string sessionID)
-        {
-            return "";
-        }
     }
 }
--- PokemonBoxTest/UtilitiesTest.cs
+++ PokemonBoxTest/UtilitiesTest.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PokemonBoxTest
+namespace PokemonBox.Test
 {
     public class UtilitiesTest
     {
