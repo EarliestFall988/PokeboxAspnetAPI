9631b0ff35c17bee6734d49b621e60daf2275f65
Emma Beatty <enbeatty@ksu.edu>
2023-04-21 13:29:40 -0500

    added to the items controller

--- PokemonBox/Controllers/Items.cs
+++ PokemonBox/Controllers/Items.cs
@@ -16,23 +16,66 @@ namespace PokemonBox.Controllers
     [ApiController]
     public class Items : ControllerBase
     {
-        //[HttpGet]
-        //public IEnumerable<string> GetAllOwnedItems() 
-        //{
-        //
-        //}
 
-        [HttpGet("api/GetAllOwnedItemsByUser")]
-        public string GetAllOwnedItemsByUser([FromHeader] string SessionId, [FromQuery] string username)
+        /*********************************
+        * 
+        * OwnedItems Methods
+        * 
+        * ******************************/
+        [HttpGet("SelectAllItemsOwned")]
+        public string SelectAllItemsOwned() 
+        {
+            IReadOnlyList<ItemsOwned> items = DatabaseConnection.ItemsOwnedRepo.SelectAllItemsOwned();
+            return JsonSerializer.Serialize(items);
+        }
+
+        [HttpGet("SelectAllItemsOwnedByUser")]
+        public string SelectAllItemsOwnedByUser([FromHeader] string SessionId, [FromQuery] string username)
         {
             IReadOnlyList<ItemsOwned> items = DatabaseConnection.ItemsOwnedRepo.SelectAllItemsOwnedByUser(username);
             return JsonSerializer.Serialize(items);
         }
 
-        //[HttpGet]
-        //public IEnumerable<string> GetAllItemTypes()
+        [HttpPost("CreateItemsOwned")]
+        public void CreateItemsOwned([FromHeader] string SessionId, [FromQuery] string username, [FromQuery] string itemName)
+        {
+            ItemsOwned item = DatabaseConnection.ItemsOwnedRepo.CreateItemsOwned(username, itemName);
+            //return JsonSerializer.Serialize(item);
+        }
+
+        [HttpPost("RemoveItemsOwned")]
+        public void RemoveItemsOwned([FromHeader] string SessionId, [FromQuery] string username, [FromQuery] string itemName)
+        {
+            DatabaseConnection.ItemsOwnedRepo.RemoveItemsOwned(username, itemName);
+            //return JsonSerializer.Serialize(item);
+        }
+
+        [HttpGet("SelectSingleItemOwned")]
+        public string SelectSingleItemOwned([FromHeader] string SessionId, [FromQuery] string username, [FromQuery] string itemName)
+        {
+            ItemsOwned item = DatabaseConnection.ItemsOwnedRepo.SelectSingleItemOwned(username, itemName);
+            return JsonSerializer.Serialize(item);
+        }
+
+        [HttpGet("TopItem")]
+        public string TopItem([FromHeader] string SessionId, [FromQuery] uint year, [FromQuery] uint month)
+        {
+            IReadOnlyDictionary<uint, uint> items = DatabaseConnection.ItemsOwnedRepo.TopItem(year, month);
+            return JsonSerializer.Serialize(items);
+        }
+
+
+
+        /*********************************
+         * 
+         * ItemTypes Methods
+         * 
+         * ******************************/
+
+        //[HttpGet("api/GetAllItemTypes")]
+        //public string GetAllItemTypes()
         //{
-        //
+        //    
         //}
     }
 }
