9216d9597edaf6ff16f682aebf7365d0826e47eb
Emma Beatty <enbeatty@ksu.edu>
2023-04-20 21:37:23 -0500

    git some stuff done

---
+++ PokemonBox/Controllers/Items.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+using PokemonBox.Models;
+using PokemonBox.SqlRepositories;
+using PokemonBox.Utils;
+
+using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace PokemonBox.Controllers
+{
+    [EnableCors("*")]
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class Items : ControllerBase
+    {
+        //[HttpGet]
+        //public IEnumerable<string> GetAllOwnedItems() 
+        //{
+        //
+        //}
+
+        [HttpGet("api/GetAllOwnedItemsByUser")]
+        public string GetAllOwnedItemsByUser([FromHeader] string SessionId, [FromQuery] string username)
+        {
+            IReadOnlyList<ItemsOwned> items = DatabaseConnection.ItemsOwnedRepo.SelectAllItemsOwnedByUser(username);
+            return JsonSerializer.Serialize(items);
+        }
+
+        //[HttpGet]
+        //public IEnumerable<string> GetAllItemTypes()
+        //{
+        //
+        //}
+    }
+}
---
+++ PokemonBox/Controllers/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+using PokemonBox.Models;
+using PokemonBox.SqlRepositories;
+using PokemonBox.Utils;
+
+using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace PokemonBox.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Pokemon : ControllerBase
+    {
+    }
+}
